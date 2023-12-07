785a57414a4692c4ee8838e8ec048ae4629b9100
Rufus Uttley <rufusuttley@live.co.uk>
2023-12-07 16:40:25 +0000

    Day 7 Part 2

--- day7/src/lib.rs
+++ day7/src/lib.rs
@@ -7,6 +7,7 @@ const CARDS: [char; 14] = [
 #[derive(Debug, Eq)]
 struct Hand {
     cards: [char; 5],
+    joker: bool,
     bid: u32,
 }
 
@@ -23,36 +24,50 @@ enum HandType {
 
 impl Hand {
     fn best_hand_type(&self) -> HandType {
-        let mut count = [0u32; 14];
-        for card in self.cards.iter() {
-            let index = Self::card_rank(*card);
-            count[index] += 1;
+        let mut counts = [0; 14];
+
+        for &card in self.cards.iter() {
+            let index = CARDS.iter().position(|&c| c == card).unwrap();
+            counts[index] += 1;
         }
 
-        if count.contains(&5) {
-            return HandType::FiveOfAKind;
+        // If use jokers as wildcards
+        if self.joker {
+            // Get max position
+            let max_index = counts
+                .iter()
+                .enumerate()
+                .skip(1)
+                .max_by_key(|(_, &count)| count)
+                .unwrap()
+                .0;
+
+            counts[max_index] += counts[0];
+            counts[0] = 0;
         }
 
-        if count.contains(&4) {
-            return HandType::FourOfAKind;
-        }
+        let max_count = *counts.iter().max().unwrap();
 
-        if count.contains(&3) {
-            if count.contains(&2) {
-                return HandType::FullHouse;
-            } else {
-                return HandType::ThreeOfAKind;
+        match max_count {
+            1 => HandType::HighCard,
+            2 => {
+                if counts.iter().filter(|&&count| count == 2).count() == 2 {
+                    HandType::TwoPair
+                } else {
+                    HandType::OnePair
+                }
             }
+            3 => {
+                if counts.iter().any(|&count| count == 2) {
+                    HandType::FullHouse
+                } else {
+                    HandType::ThreeOfAKind
+                }
+            }
+            4 => HandType::FourOfAKind,
+            5 => HandType::FiveOfAKind,
+            _ => unreachable!(),
         }
-
-        let pair_count = count.iter().filter(|c| **c == 2).count();
-        if pair_count == 2 {
-            return HandType::TwoPair;
-        } else if pair_count == 1 {
-            return HandType::OnePair;
-        }
-
-        HandType::HighCard
     }
 
     fn card_rank(card: char) -> usize {
@@ -87,7 +102,7 @@ impl PartialEq for Hand {
     }
 }
 
-fn parse_hands(input: &str) -> Vec<Hand> {
+fn parse_hands(input: &str, joker: bool) -> Vec<Hand> {
     input
         .trim()
         .lines()
@@ -97,20 +112,19 @@ fn parse_hands(input: &str) -> Vec<Hand> {
             // Collect into temp vec before convert to array
             let cards: [char; 5] = cards
                 .chars()
-                .into_iter()
                 .collect::<Vec<char>>()
                 .try_into()
                 .expect("Expect 5 cards");
 
             let bid = bid.parse::<u32>().expect("Invalid bid");
 
-            Hand { cards, bid }
+            Hand { cards, joker, bid }
         })
         .collect()
 }
 
 pub fn part1(input: &str) -> u32 {
-    let mut hands = parse_hands(input);
+    let mut hands = parse_hands(input, false);
 
     // Sort hands
     hands.sort_unstable();
@@ -123,14 +137,23 @@ pub fn part1(input: &str) -> u32 {
 }
 
 pub fn part2(input: &str) -> u32 {
-    todo!()
+    let mut hands = parse_hands(input, true);
+
+    // Sort hands
+    hands.sort_unstable();
+
+    hands
+        .iter()
+        .enumerate()
+        .map(|(i, hand)| (i + 1) as u32 * hand.bid)
+        .sum::<u32>()
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    const TEST_PART1: &str = r#"
+    const TEST_INPUT: &str = r#"
 32T3K 765
 T55J5 684
 KK677 28
@@ -138,17 +161,9 @@ KTJJT 220
 QQQJA 483
 "#;
 
-    const TEST_PART2: &str = r#"
-32T3K 765
-T55J5 684
-KK677 28
-KTJJT 220
-QQQJA 483 
-"#;
-
     #[test]
     fn test_hand_type() {
-        let hand_types: Vec<_> = TEST_PART1
+        let test_hands: Vec<_> = TEST_INPUT
             .trim()
             .lines()
             .map(|line| {
@@ -156,23 +171,48 @@ QQQJA 483
 
                 let cards: [char; 5] = cards
                     .chars()
-                    .into_iter()
                     .collect::<Vec<char>>()
                     .try_into()
                     .expect("Expect 5 cards");
 
-                Hand { cards, bid: 0 }.best_hand_type()
+                Hand {
+                    cards,
+                    joker: false,
+                    bid: 0,
+                }
             })
             .collect();
 
         assert_eq!(
-            hand_types,
+            test_hands
+                .iter()
+                .map(Hand::best_hand_type)
+                .collect::<Vec<_>>(),
             [
                 HandType::OnePair,
                 HandType::ThreeOfAKind,
                 HandType::TwoPair,
                 HandType::TwoPair,
-                HandType::ThreeOfAKind
+                HandType::ThreeOfAKind,
+            ]
+        );
+
+        assert_eq!(
+            test_hands
+                .iter()
+                .map(|hand| Hand {
+                    cards: hand.cards,
+                    joker: true,
+                    bid: 0
+                })
+                .map(|hand| hand.best_hand_type())
+                .collect::<Vec<_>>(),
+            [
+                HandType::OnePair,
+                HandType::FourOfAKind,
+                HandType::TwoPair,
+                HandType::FourOfAKind,
+                HandType::FourOfAKind,
             ]
         );
     }
@@ -181,30 +221,36 @@ QQQJA 483
     fn test_cmp_hands() {
         let a = Hand {
             cards: ['3', '3', '3', '3', '2'],
+            joker: false,
             bid: 0,
         };
         let b = Hand {
             cards: ['2', 'A', 'A', 'A', 'A'],
+            joker: false,
             bid: 0,
         };
         assert_eq!(a.cmp(&b), Ordering::Greater);
 
         let a = Hand {
             cards: ['7', '7', '8', '8', '8'],
+            joker: false,
             bid: 0,
         };
         let b = Hand {
             cards: ['7', '7', '7', '8', '8'],
+            joker: false,
             bid: 0,
         };
         assert_eq!(a.cmp(&b), Ordering::Greater);
 
         let a = Hand {
             cards: ['K', 'K', '6', '7', '7'],
+            joker: false,
             bid: 0,
         };
         let b = Hand {
             cards: ['K', 'T', 'J', 'J', 'T'],
+            joker: false,
             bid: 0,
         };
         assert_eq!(a.cmp(&b), Ordering::Greater);
@@ -212,11 +258,11 @@ QQQJA 483
 
     #[test]
     fn test_part1() {
-        assert_eq!(part1(TEST_PART1), 6440);
+        assert_eq!(part1(TEST_INPUT), 6440);
     }
 
-    // #[test]
-    // fn test_part2() {
-    //     assert_eq!(part2(TEST_INPUT), 71503);
-    // }
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2(TEST_INPUT), 5905);
+    }
 }
