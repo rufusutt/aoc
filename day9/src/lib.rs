25b36e8c83e28281401ae8008cc2d06bb3672aa5
Rufus Uttley <rufusuttley@live.co.uk>
2023-12-09 18:17:40 +0000

    Day 9 binomial transform coefficients

--- day9/src/lib.rs
+++ day9/src/lib.rs
@@ -10,67 +10,72 @@ fn parse_sequences(input: &str) -> Vec<Vec<i32>> {
         .collect()
 }
 
-fn build_difference_triangle(sequence: &[i32]) -> Vec<Vec<i32>> {
-    let mut differences: Vec<Vec<i32>> = vec![sequence.to_vec()];
+/// Generates coefficients for the nth difference operation based on Pascal's triangle.
+///
+/// Given an integer `n`, this function computes the coefficients used in the formula for the nth
+/// difference operation. The formula involves binomial coefficients with alternating signs,
+/// resembling a signed version of Pascal's triangle.
+fn generate_coefficients(n: usize) -> Vec<i32> {
+    let mut coefficients = vec![1];
 
-    for n in 0..sequence.len() - 1 {
-        let last = &differences[n];
-
-        differences.push(
-            (0..(sequence.len() - n - 1))
-                .map(|i| last[i + 1] - last[i])
-                .collect(),
-        );
+    for k in 1..=n {
+        let next_coefficient = coefficients[k - 1] * (n as i32 - k as i32 + 1) / k as i32;
+        coefficients.push(next_coefficient);
     }
 
-    differences
+    // Apply alternating signs
+    for coefficient in coefficients.iter_mut().skip(1).step_by(2) {
+        *coefficient *= -1;
+    }
+
+    coefficients
+}
+
+/// Interpolates a sequence of equally spaced points using a polynomial of degree at most n,
+/// where n is the length of the input sequence minus 1.
+/// The interpolation is based on the method of finite differences.
+///
+/// A polynomial y(x) of degree d defines a sequence of values at positive integer points,
+/// y_j = y(j), and the (d + 1)th difference of this sequence is identically zero:
+/// Δ^(d+1)y = 0.
+///
+/// Thus, given values y_0, ..., y_n at equally spaced points, where n = d + 1, we have:
+/// (-1)^n * y_0 + (-1)^(n-1) * C(n,1) * y_1 + ... - C(n,n-1) * y_(n-1) + y_n = 0.
+fn interpolate_sequence(sequence: &[i32]) -> i32 {
+    let coefficients = generate_coefficients(sequence.len());
+
+    let interpolation = sequence
+        .iter()
+        .zip(coefficients.iter().rev())
+        .map(|(x, coeff)| x * coeff)
+        .sum::<i32>();
+
+    interpolation * -1
 }
 
 pub fn part1(input: &str) -> u32 {
     let sequences = parse_sequences(input);
-    let mut sum = 0;
 
-    for sequence in sequences {
-        let mut differences = build_difference_triangle(&sequence);
-
-        // Initial case
-        differences[sequence.len() - 1].push(0);
-
-        for i in (0..sequence.len() - 2).rev() {
-            let last = *differences[i].last().unwrap();
-            let delta = *differences[i + 1].last().unwrap();
-
-            differences[i].push(last + delta);
-        }
-
-        sum += differences[0].last().unwrap();
-    }
-
-    sum.try_into().unwrap()
+    sequences
+        .into_iter()
+        .map(|s| interpolate_sequence(&s))
+        .sum::<i32>()
+        .try_into()
+        .expect("Runner needs unsigned answer")
 }
 
 pub fn part2(input: &str) -> u32 {
-    let sequences = parse_sequences(input);
-    let mut sum = 0;
+    let mut sequences = parse_sequences(input);
 
-    for mut sequence in sequences {
-        sequence.reverse();
-        let mut differences = build_difference_triangle(&sequence);
+    // Reverse all sequences
+    sequences.iter_mut().for_each(|s| s.reverse());
 
-        // Initial case
-        differences[sequence.len() - 1].push(0);
-
-        for i in (0..sequence.len() - 2).rev() {
-            let last = *differences[i].last().unwrap();
-            let delta = *differences[i + 1].last().unwrap();
-
-            differences[i].push(last + delta);
-        }
-
-        sum += differences[0].last().unwrap();
-    }
-
-    sum.try_into().unwrap()
+    sequences
+        .into_iter()
+        .map(|s| interpolate_sequence(&s))
+        .sum::<i32>()
+        .try_into()
+        .expect("Runner needs unsigned answer")
 }
 
 #[cfg(test)]
